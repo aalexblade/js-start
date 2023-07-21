42cafa35175f3fcb1fd599288dc408fd00754c7f
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-07-21 13:03:16 +0300

    crud

--- practics2.js
+++ practics2.js
@@ -71,18 +71,38 @@
 // Доповни код функції createArrayOfNumbers(min, max) таким чином,
 // щоб вона повертала масив усіх цілих чисел від значення min до max.
 
-function createArrayOfNumbers(min, max) {
-    const numbers = [];
+
+// function createArrayOfNumbers(min, max) {
+//     const numbers = []
+//     for (let i = min; i <= max; i += 1) {
+//         numbers.push(i)
+//     }
+//     return numbers
+// }
+
+// console.log(createArrayOfNumbers(1, 3))
+// console.log(createArrayOfNumbers(14, 17))
+// console.log(createArrayOfNumbers(29, 33))
+
+// ===========================================================
+// Напиши функцію filterArray(numbers, value), яка приймає масив чисел (параметр numbers)
+// і повертає новий масив, в якому будуть тільки ті елементи масиву numbers, які більші
+// за значення параметра value (число).
+
+function filterArray(numbers, value) {
     // Change code below this line
-    for (let i = min; i <= max; i += 1) {
-        numbers.push(i);
+    const newArray = [];
 
+
+    for (let i = 0; i < numbers.length; i += 1) {
+
+        if (numbers[i] > value) {
+            newArray.push(numbers[i])
+
+        }
+        // Change code above this 
     }
+    return newArray;
 
-    // Change code above this line
-    return numbers;
 }
-
-console.log(createArrayOfNumbers(1, 3))
-console.log(createArrayOfNumbers(14, 17))
-console.log(createArrayOfNumbers(29, 33))
+console.log(filterArray([1, 2, 3, 4, 5], 3))
