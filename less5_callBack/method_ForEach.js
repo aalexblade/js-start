6730ca9c6eeeb99d5af1e8bea16030bc15c13882
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-09-14 21:47:03 +0300

    less

--- less5_callBack/method_ForEach.js
+++ less5_callBack/method_ForEach.js
@@ -18,13 +18,15 @@ const numbers = [5, 10, 15, 20, 25];
 
 // Класичний for
 for (let i = 0; i < numbers.length; i += 1) {
-    console.log(`Індекс ${i}, значення ${numbers[i]}`);
+    // console.log(`Індекс ${i}, значення ${numbers[i]}`);
 }
 
 // Метод перебирання forEach
 numbers.forEach(function (number, index) {
-    console.log(`Індекс ${index}, значення ${number}`);
+    // console.log(`Індекс ${index}, значення ${number}`);
+
 });
 // Єдиним випадком, коли варто використовувати цикли for або for...of для перебирання
 // масиву, - це задачі з перериванням виконання циклу. Перервати виконання методу forEach не
 // можна, він завжди перебирає масив до кінця.
+
