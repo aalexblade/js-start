39e85d4b76ce246714e155ab6c4ed190e238f7d4
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-09-25 12:55:02 +0300

    practicaseChaining

--- practice.js
+++ practice.js
@@ -317,3 +317,121 @@
 // console.log(sortedByAscendingRating);
 // console.log(sortedByDescentingRating);
 
+
+// const users = [
+//     {
+//         name: "Moore Hensley",
+//         email: "moorehensley@indexia.com",
+//         eyeColor: "blue",
+//         friends: ["Sharron Pace"],
+//         isActive: false,
+//         balance: 2811,
+//         gender: "male"
+//     },
+//     {
+//         name: "Sharlene Bush",
+//         email: "sharlenebush@tubesys.com",
+//         eyeColor: "blue",
+//         friends: ["Briana Decker", "Sharron Pace"],
+//         isActive: true,
+//         balance: 3821,
+//         gender: "female"
+//     },
+//     {
+//         name: "Ross Vazquez",
+//         email: "rossvazquez@xinware.com",
+//         eyeColor: "green",
+//         friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//         isActive: false,
+//         balance: 3793,
+//         gender: "male"
+//     },
+//     {
+//         name: "Elma Head",
+//         email: "elmahead@omatom.com",
+//         eyeColor: "green",
+//         friends: ["Goldie Gentry", "Aisha Tran"],
+//         isActive: true,
+//         balance: 2278,
+//         gender: "female"
+//     },
+//     {
+//         name: "Carey Barr",
+//         email: "careybarr@nurali.com",
+//         eyeColor: "blue",
+//         friends: ["Jordan Sampson", "Eddie Strong"],
+//         isActive: true,
+//         balance: 3951,
+//         gender: "male"
+//     },
+//     {
+//         name: "Blackburn Dotson",
+//         email: "blackburndotson@furnigeer.com",
+//         eyeColor: "brown",
+//         friends: ["Jacklyn Lucas", "Linda Chapman"],
+//         isActive: false,
+//         balance: 1498,
+//         gender: "male"
+//     },
+//     {
+//         name: "Sheree Anthony",
+//         email: "shereeanthony@kog.com",
+//         eyeColor: "brown",
+//         friends: ["Goldie Gentry", "Briana Decker"],
+//         isActive: true,
+//         balance: 2764,
+//         gender: "female"
+//     }
+// ]
+
+// const sortedByDescentingRating = [...users].sort(
+//     (firstRating, secondRating) =>
+//         secondRating.friends.length - firstRating.friends.length
+// )
+
+
+// console.log(sortedByDescentingRating)
+
+// ============================================
+// chaining
+
+// Доповни код таким чином, щоб у змінній names вийшов масив імен
+// авторів в алфавітному порядку, рейтинг книг яких більший за
+// значення змінної MIN_BOOK_RATING.
+
+// .filtre() - фільтруємо масив за рейтенгом
+// .sort() - сортуємо новий в масив імен авторів в алфавітному порядку
+// .map() - витягуємо з масива імена
+
+
+// const books = [
+//     {
+//         title: "The Last Kingdom",
+//         author: "Bernard Cornwell",
+//         rating: 8.38,
+//     },
+//     {
+//         title: "Beside Still Waters",
+//         author: "Robert Sheckley",
+//         rating: 8.51,
+//     },
+//     {
+//         title: "The Dream of a Ridiculous Man",
+//         author: "Fyodor Dostoevsky",
+//         rating: 7.75,
+//     },
+//     { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//     {
+//         title: "The Dreams in the Witch House",
+//         author: "Howard Lovecraft",
+//         rating: 8.67,
+//     },
+// ];
+// const MIN_BOOK_RATING = 8;
+
+// const names = [...books]
+//     .filter(book => book.rating >= MIN_BOOK_RATING)
+//     .sort((a, b) => a.author.localeCompare(b.author))
+//     .map(book => book.author)
+
+// console.log(names)
