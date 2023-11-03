5ab6b82fe0920f878acce0e159fafadd571043a5
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-11-03 17:29:15 +0200

    hwT01

---
+++ homeWork6/js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesRef = document.querySelector('#categories')
+const itemsRef = document.querySelectorAll('.item')
+
+// console.log(categoriesRef)
+console.log(itemsRef)
+
+// const murkup = categoriesRef.forEach((categorie) => `Number of categories:${categorie}`)
+// // .join('')
+
+// console.log(murkup)
