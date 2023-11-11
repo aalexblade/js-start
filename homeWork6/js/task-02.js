d1bb7953a4ae3703bef796500ffbf00ea6e159f3
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-11-11 17:07:44 +0200

    task3

--- homeWork6/js/task-02.js
+++ homeWork6/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients')
+
+const itemIngredients = ingredients.map((ingredient) => {
+  const liRef = document.createElement("li")
+  liRef.textContent = ingredient
+  liRef.classList.add('item')
+
+  return liRef
+})
+
+
+
+ingredientsRef.append(...itemIngredients)
+
+// ingredientsRef.append(itemIngredients)
--- homeWork6/js/task-03.js
+++ homeWork6/js/task-03.js
@@ -12,3 +12,15 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryRef = document.querySelector('.gallery')
+
+const markup = images.map(({ url, alt }) =>
+  `<li>
+  <img src='${url}' alt='${alt}' width = "300" height = "auto"/>
+  </li>`
+).join('')
+
+galleryRef.insertAdjacentHTML('beforeend', markup)
+
+console.log(galleryRef)
