b5762e146e20e12308a03c41e11321ea483bcb15
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-11-12 13:35:42 +0200

    hwTask5

---
+++ homeWork6/js/task-05.js
@@ -0,0 +1,26 @@
+const textInput = document.querySelector('#name-input')
+const output = document.querySelector('#name-output')
+
+textInput.addEventListener('input', (event) => {
+    output.textContent = event.currentTarget.value || 'Anonymous'
+
+})
+
+// ====================================================
+// textInput.addEventListener('input', (event) => {
+//     // output.textContent = event.currentTarget.value
+
+//     if (output.textContent = event.currentTarget.value) {
+//         output.textContent
+//     } else {
+//         output.textContent = 'Anonymous'
+//     }
+
+// })
+// ==================================================
+// const createInputName = (event) => {
+//     event.currentTarget.value === ""
+//         ? (output.textContent = "Anonymous")
+//         : (output.textContent = event.currentTarget.value);
+// };
+// textInput.addEventListener("input", createInputName);
