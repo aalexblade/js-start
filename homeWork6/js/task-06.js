f568d8d2048fbc9241cfe472f0c872cf78a7028a
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-11-17 11:35:58 +0200

    hwTask6

--- homeWork6/js/task-06.js
+++ homeWork6/js/task-06.js
@@ -1,29 +1,17 @@
-// const validTextInput = document.querySelector('#validation-input')
-// const validInput = document.querySelector('.valid')
-// console.log(validInput)
-
-// validTextInput.addEventListener('blur', () => {
-//     // console.log(validTextInput.value)
-//     const text = validTextInput.value
-//     const valid = validTextInput.dataset
-
-//     if (text === valid) {
-
-//     }
-
-// })
 
 const inputRef = document.querySelector('#validation-input')
 const numberOfSymbol = inputRef.dataset.length
 
-function verifyValidation(event) {
+inputRef.addEventListener('blur', validation)
+
+function validation(event) {
+
     if (event.currentTarget.value.length == numberOfSymbol) {
-        inputRef.classList.remove('invalid');
-        return inputRef.classList.add('valid');
-    } else {
-        inputRef.classList.remove('valid');
-        return inputRef.classList.add('invalid');
+        inputRef.classList.remove('invalid')
+        return inputRef.classList.add('valid')
+    }
+    else {
+        inputRef.classList.remove('valid')
+        return inputRef.classList.add('invalid')
     }
 }
-
-inputRef.addEventListener('blur', verifyValidation);
