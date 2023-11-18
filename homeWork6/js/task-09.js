cad0b0b614d46e25ab112638c7325f18103afd4b
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-11-18 12:37:31 +0200

    hwTask09

--- homeWork6/js/task-09.js
+++ homeWork6/js/task-09.js
@@ -1,5 +1,18 @@
+
+const bodyChangeColor = document.querySelector('body')
+const bodyRef = document.querySelector('.color')
+
+bodyChangeColor.addEventListener('click', onChangeColor)
+
+function onChangeColor(event) {
+  bodyChangeColor.style.backgroundColor = getRandomHexColor()
+  bodyRef.textContent = bodyChangeColor.style.backgroundColor
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
