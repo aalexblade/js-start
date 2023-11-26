f17aa6ef30966361f272c053a533f222eb6993f1
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-11-26 20:34:04 +0200

    hw-07

--- homeWork6/js/task-09.js
+++ homeWork6/js/task-09.js
@@ -1,18 +1,33 @@
 
-const bodyChangeColor = document.querySelector('body')
-const spanColor = document.querySelector('.color')
+const bodyEl = document.body;
+const buttonEl = document.querySelector(".change-color");
+const nameColorEl = document.querySelector(".color");
 
-bodyChangeColor.addEventListener('click', onChangeColor)
+buttonEl.addEventListener("click", changeBgColor);
 
-function onChangeColor(event) {
-  bodyChangeColor.style.backgroundColor = getRandomHexColor()
-  spanColor.textContent = bodyChangeColor.style.backgroundColor
+function changeBgColor() {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  nameColorEl.textContent = randomColor;
 }
-
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 
+
+// const bodyChangeColor = document.querySelector('body')
+// const spanColor = document.querySelector('.color')
+
+// bodyChangeColor.addEventListener('click', onChangeColor)
+
+// function onChangeColor(event) {
+//   bodyChangeColor.style.backgroundColor = getRandomHexColor()
+//   spanColor.textContent = bodyChangeColor.style.backgroundColor
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
