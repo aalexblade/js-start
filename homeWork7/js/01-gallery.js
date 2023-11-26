4dc485be6ad010e91be1627c021ea0071011cb18
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-11-26 20:34:28 +0200

    hw07

---
+++ homeWork7/js/01-gallery.js
@@ -0,0 +1,42 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+
+const markup = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+)
+
+gallery.insertAdjacentHTML('beforeend', markup.join(''))
+gallery.addEventListener('click', onClick)
+
+function onClick(evt) {
+    evt.preventDefault()
+    if (!evt.target.classList.contains('gallery__image')) {
+        return
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">
+    `)
+    instance.show()
+
+    document.addEventListener('keydown', (eventKeyboard) => {
+        if (eventKeyboard.key === 'Escape') instance.close();
+    });
+
+}
+
+
+
+
